dbd7afb201ab9f8001de287c16ea908709d7c013
Sahaj <sahaj.bhaikanhaiya@gmail.com>
2015-10-29 13:49:00 +0530

    bomb prefab

--- Assets/Code/Controllers/BombBehaviour.cs
+++ Assets/Code/Controllers/BombBehaviour.cs
@@ -3,30 +3,49 @@ using System.Collections;
 using Assets.Code.DataPipeline;
 using Assets.Code.UnityBehaviours.Pooling;
 using Assets.Code.UnityBehaviours;
+using Assets.Code.Logic.Pooling;
 
 public class BombBehaviour : PoolingBehaviour{
 
     BombModel model;
+    private float startTime;
+    private float journeyLength;
+    private float speed = 5.0F;
+    private Vector3 _startPos;
+    private Vector3 _target;
+    private PoolingParticleManager _poolingParticleManager;
+
     public void Initialize(IoCResolver resolver, BombModel model)
     {
         this.model = model;
+        resolver.Resolve(out _poolingParticleManager);
 
+        gameObject.GetComponent<Renderer>().material.color = model.color;
+        _startPos = model.startPos;
+        _target = model.endPos;
 
+        journeyLength = Vector3.Distance(_startPos, _target);
+
+        _poolingParticleManager.Emit("blood_prefab", transform.position, Color.green, 200);
     }
 
-    //void Update()
-    //{
-    //    float distCovered = (Time.time - startTime) * speed;
-    //    float fracJourney = (distCovered / journeyLength) * 100;
+    void Update() {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = (distCovered / journeyLength) * 100;
+  
+        transform.position = Vector3.Lerp(_startPos, _target, fracJourney);
+    }
 
-    //    if (_hit)
-    //    {
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Cube") {
+            //Damage Building
+            Destroy(this.gameObject);
 
-    //        transform.position = Vector3.Lerp(_startPos, _target, fracJourney);
-    //    }
-    //    else
-    //    {
-    //        transform.position = Vector3.Lerp(_startPos, _target + randomPosition, fracJourney);
-    //    }
-    //}
+        }
+    }
+
+
 }
+  
+
