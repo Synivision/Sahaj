9bcc2d992fc94c7c0cad0aebbad5ba309f0cdcdd
sahaj.bhaikanhaiya <sahaj.bhaikanhaiya@gmail.com>
2016-01-15 22:12:56 +0530

    Create pirate canvas added

---
+++ Assets/Code/CanvasControllers/CreatePirateCanvasController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using Assets.Code.DataPipeline;
+using Assets.Code.DataPipeline.Providers;
+using Assets.Code.Messaging;
+using Assets.Code.Messaging.Messages;
+using Assets.Code.UnityBehaviours;
+using Assets.Code.States;
+
+namespace Assets.Code.Ui.CanvasControllers
+{
+	public class CreatePirateCanvasController : BaseCanvasController
+	{
+		//Scroll Views
+		private GameObject _parentPirateButtonPanel;
+		private GameObject _parentPirateImagesScrollViewPanel;
+
+		//Time left Label
+		private Text timeLeftText;
+
+		//Navigation Buttons
+		private Button leftNavigationButton;
+		private Button rightNavigationButton;
+
+		public CreatePirateCanvasController(IoCResolver resolver, Canvas canvasView)
+		: base(resolver, canvasView){
+
+
+
+		}
+
+	}
+}
